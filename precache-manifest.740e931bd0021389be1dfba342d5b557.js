4c5f3441a44f714071cbfe72b78186b11319df4d
Huggo Parcelly <h.parcelly@gmail.com>
2021-10-04 19:10:09 -0300

    Updates

---
+++ precache-manifest.740e931bd0021389be1dfba342d5b557.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c651d86f39234779719fb2b2af5728",
+    "url": "/movie-card-crud/index.html"
+  },
+  {
+    "revision": "31ce1dab7679094e4c51",
+    "url": "/movie-card-crud/static/css/main.321d70e3.chunk.css"
+  },
+  {
+    "revision": "c18d5bd387ea977d735a",
+    "url": "/movie-card-crud/static/js/2.cd8e7c5c.chunk.js"
+  },
+  {
+    "revision": "31ce1dab7679094e4c51",
+    "url": "/movie-card-crud/static/js/main.e59b832e.chunk.js"
+  },
+  {
+    "revision": "7582370dd4f02f9c63fe",
+    "url": "/movie-card-crud/static/js/runtime-main.bdb2e81f.js"
+  }
+]);
